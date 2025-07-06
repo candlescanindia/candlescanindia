ecd10dc9982bb2afd85e60027261935835c01fbd
candlescanindia <candlescanindia@gmail.com>
2025-07-07 01:59:00 +0530

    Delete utils/nse_data.py

--- utils/nse_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_stocks():
-    try:
-        df = pd.read_csv("data/nse_stock_list.csv")
-        return df
-    except FileNotFoundError:
-        raise FileNotFoundError("nse_stock_list.csv file not found in /data folder.")
